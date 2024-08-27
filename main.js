73e98ea45a81c8a9907198a052cd9b907bce4029
Igor2693 <i.bugaytsov@mail.ru>
2024-08-27 17:44:00 +0300

    выполнение урока №16

--- main.js
+++ main.js
@@ -1,41 +1,26 @@
 "use strict";
-const DomElement = function (selector,height,width,bg,fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
 
+"use strict";
 
-}
-
-DomElement.prototype.createElem = function() {
-  let item 
-  console.log(item)
-  if (this.selector.startsWith(".")) {
-    item = document.createElement('div');
-    item.classList.add(this.selector.slice(1));
-     document.body.append(item);
-   
-  } else if (this.selector.startsWith("#")) {
-    item = document.createElement('p');
-    item.setAttribute('id', this.selector.slice(1));
-  
-    document.body.append(item);
+class First {
+  constructor () {
+    
   }
-
-  item.style.cssText = `
-  height: ${this.height}px;
-  width: ${this.width}px;
-  background: ${this.bg};
-  font-size: ${this.fontSize}px;`;
-  item.textContent = 'Элемент создан'
+  hello() {
+    console.log('Привет я метод родителя!')
+  }
 }
 
+class Second extends First {
+  constructor () {
+    super()
+  }
+  hello() {
+    super.hello()
+    console.log('А я следуемый метод!')
+  }
+}
 
-const newItem1 = new DomElement('#best', "200", "200", "tomato", "20");
+const run = new Second()
 
-const newItem2 = new DomElement('.block', "200", "200", "blue", "20");
-
-newItem1.createElem()
-newItem2.createElem()
+run.hello()
